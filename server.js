2d896caf4150b1674115e78946c73d6f3aaa8e46
Study Only <0xAxy>
2023-06-08 23:36:33 +1000

    Removed bcrypt dependencies

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // importing packages
 const express = require('express');
 const admin = require('firebase-admin');
-const bcrypt = require('bcrypt');
 const path = require('path');
 const nodemailer = require('nodemailer');
 
@@ -73,80 +72,14 @@ app.get('/signup', (req, res) => {
     res.sendFile(path.join(staticPath, "signup.html"));
 })
 
-app.post('/signup', (req, res) => {
-    let { name, email, password, number, tac, notification } = req.body;
 
-    // form validations
-    if(name.length < 3){
-        return res.json({'alert': 'name must be 3 letters long'});
-    } else if(!email.length){
-        return res.json({'alert': 'enter your email'});
-    } else if(password.length < 8){
-        return res.json({'alert': 'password should be 8 letters long'});
-    } else if(!number.length){
-        return res.json({'alert': 'enter your phone number'});
-    } else if(!Number(number) || number.length < 10){
-        return res.json({'alert': 'invalid number, please enter valid one'});
-    } else if(!tac){
-        return res.json({'alert': 'you must agree to our terms and conditions'});
-    } 
-    
-    // store user in db
-    db.collection('users').doc(email).get()
-    .then(user => {
-        if(user.exists){
-            return res.json({'alert': 'email already exists'});
-        } else{
-            // encrypt the password before storing it.
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(password, salt, (err, hash) => {
-                    req.body.password = hash;
-                    db.collection('users').doc(email).set(req.body)
-                    .then(data => {
-                        res.json({
-                            name: req.body.name,
-                            email: req.body.email,
-                            seller: req.body.seller,
-                        })
-                    })
-                })
-            })
-        }
-    })
-})
 
 // login route
 app.get('/login', (req, res) => {
     res.sendFile(path.join(staticPath, "login.html"));
 })
 
-app.post('/login', (req, res) => {
-    let { email, password } = req.body;
 
-    if(!email.length || !password.length){
-        return res.json({'alert': 'fill all the inputs'})
-    }
-
-    db.collection('users').doc(email).get()
-    .then(user => {
-        if(!user.exists){ // if email does not exists
-            return res.json({'alert': 'log in email does not exists'})
-        } else{
-            bcrypt.compare(password, user.data().password, (err, result) => {
-                if(result){
-                    let data = user.data();
-                    return res.json({
-                        name: data.name,
-                        email: data.email,
-                        seller: data.seller,
-                    })
-                } else{
-                    return res.json({'alert': 'password is incorrect'});
-                }
-            })
-        }
-    })
-})
 
 // seller route
 app.get('/seller', (req, res) => {
